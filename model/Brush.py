1b2fcac63e56ec50ce4e33583bd65beb74bcba25
Benedikt Ruhland <beneruhland@gmail.com>
2023-02-27 22:33:49 +0100

    update model

--- model/Brush.py
+++ model/Brush.py
@@ -6,6 +6,7 @@ import numpy as np
 
 #----------------------------------------------------------------
 #lonitudinal Fx
+#https://skill-lync.com/student-projects/Longitudinal-brush-tire-model-50471
 #----------------------------------------------------------------
 
 #Parameters
@@ -17,20 +18,31 @@ import numpy as np
 
 def brush_long(Sx,Fz,mu,lt,kt,lam):
 
+    Sx = Sx * 10 #scaling factor
     #critical slip
     Sx_crit = mu * Fz / (kt * lt * (lt + lam))
 
     Fx = np.zeros(len(Sx))
+    lc = np.zeros(len(Sx)) 
 
+    
     for i in range(0,len(Sx)):
-        if Sx[i] < Sx_crit:
-            Fx[i] = kt * lt * (lam + lt / 2) * Sx[i]
-            lc[i] = lt
+        
+        if Sx[i] >= 0:
+            if Sx[i] < Sx_crit:
+                Fx[i] = kt * lt * (lam + lt / 2) * Sx[i]
+                lc[i] = lt
+            else:
+                lc[i] = mu * Fz / (lt * kt * Sx[i]) - lam
+                Fx[i] = kt * Sx[i] * (lc[i]) * (lam + lc[i] / 2) + mu * Fz * (1 - lc[i] / lt)
         else:
-            lc[i] = mu * Fz / (lt * kt * Sx[i]) - lam
-            Fx[i] = kt * Sx[i] * (lc[i]) * (lam + lc[i] / 2) + mu * Fz * (a - lc[i] / lt)
-
-    return Fx
+            if abs(Sx[i]) < Sx_crit:
+                Fx[i] = - kt * lt * (lam + lt / 2) * abs(Sx[i])
+                lc[i] = lt
+            else:
+                lc[i] =  mu * Fz / (lt * kt * abs(Sx[i])) - lam
+                Fx[i] = - (kt * abs(Sx[i]) * (lc[i]) * (lam + lc[i] / 2) + mu * Fz * (1 - lc[i] / lt))
+    return Fx, lc
 
 #----------------------------------------------------------------
 #lateral Fy
@@ -39,7 +51,7 @@ def brush_long(Sx,Fz,mu,lt,kt,lam):
 #Parameters
 # Fz = normal Force [N]
 # mu = peak road tire fricton coefficient
-# Ca = lateral stiffness
+# Ca = lateral stiffness [N /rad]
 # lt # contact patch length
 
 # Sy in degrees
@@ -51,9 +63,15 @@ def Brush_lat(Sy, Fz, mu, Ca, lt):
 
     Fy = np.zeros(len(Sy))
     for i in range(0, len(Sy)):
-        if Sy[i] < Sy_crit:
-            Fy[i] = Ca * Sy[i]
+
+        if Sy[i] >= 0:
+            if Sy[i] < Sy_crit:
+                Fy[i] = Ca * Sy[i]
+            else:
+                Fy[i] = mu * Fz - (mu * Fz)**2 / (4 * Ca * np.tan(Sy[i]))
         else:
-            Fy[i] = mu * Fz - (mu * Fz)**2 / (4 * Ca * np.tan(Sy[i]))
-    
+            if abs(Sy[i]) < Sy_crit:
+                Fy[i] = - Ca * abs(Sy[i])
+            else:
+                Fy[i] = - (mu * Fz - (mu * Fz)**2 / (4 * Ca * np.tan(abs(Sy[i]))))
     return Fy
